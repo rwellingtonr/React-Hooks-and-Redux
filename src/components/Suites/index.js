3aebcc4460f86df881e530aaa9b2f2b318b570fa
rwellingtonr <wellington-158@hotmail.com>
2021-09-11 15:35:38 -0300

    Styled Suite list

--- src/components/Suites/index.js
+++ src/components/Suites/index.js
@@ -6,16 +6,26 @@ const Suites = () => {
   const suites = useSelector((state) => state.fetchData.users)
 
   return (
-    <section className="bg-light m-3 p-2 border border-dark rounded-3">
-      <h2>Suite</h2>
-      <label htmlFor="suites">Choose one Suite</label>
-      <input list="suitesList" type="text" name="suites" id="suites" />
-      <datalist id="suitesList">
-        {suites.map((suite) => (
-          <option key={suite.id} value={suite.address.suite}></option>
-        ))}
-      </datalist>
-    </section>
+    <div className="bg-light m-3 p-2 border border-2 border-dark rounded-3">
+      <section>
+        <h2>Suite</h2>
+        <label className="fs-5 fw-normal me-2" htmlFor="suites">
+          Choose a Suite
+        </label>
+        <input
+          className="col-3"
+          list="suitesList"
+          type="text"
+          name="suites"
+          id="suites"
+        />
+        <datalist id="suitesList">
+          {suites.map((suite) => (
+            <option key={suite.id} value={suite.address.suite}></option>
+          ))}
+        </datalist>
+      </section>
+    </div>
   )
 }
 export default Suites
