b7a42da42513ac03ca25dadf82011790e3bdf0cd
rwellingtonr <wellington-158@hotmail.com>
2021-09-11 15:56:32 -0300

    Styled Cite Content

--- src/components/Cite/index.js
+++ src/components/Cite/index.js
@@ -4,7 +4,7 @@ const Cite = () => {
   return (
     <div className="bg-light m-3 p-2 border border-2 border-dark rounded-3">
       <h2>Pale Blue Dot</h2>
-      <blockquote>
+      <blockquote className="fs-6 justified ps-2 mb-1">
         Look again at that dot. That's here. That's home. That's us. On it
         everyone you love, everyone you know, everyone you ever heard of, every
         human being who ever was, lived out their lives. The aggregate of our
@@ -17,7 +17,7 @@ const Cite = () => {
         sinner in the history of our species lived there--on a mote of dust
         suspended in a sunbeam.
       </blockquote>
-      <cite>Carl Sagan, Pale Blue Dot, 1994</cite>
+      <cite className="fs-5 ps-2">Carl Sagan, Pale Blue Dot, 1994</cite>
       <br />
     </div>
   )
